bf0dacfb889292f3a1717fc4c6e22015efcd6c31
honey_honeybee <100987659+pingping2ing@users.noreply.github.com>
2024-03-28 02:11:28 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M;
+map<string, int> clo;
+string s, c;
+
+int main()
+{
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        clo.clear();
+        cin >> M;
+        for (int j = 0; j < M; j++)
+        {
+            cin >> s >> c;
+            clo[c]++;
+        }
+        int num = 1;
+        for (auto j : clo)
+            num *= j.second + 1;
+        cout << num - 1 << "\n";
+    }
+    if (N == 0)
+        cout << 0;
+}
