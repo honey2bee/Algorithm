82b4b9a133d3a725bcda9e73b0c1300cec56e270
honey_honeybee <100987659+pingping2ing@users.noreply.github.com>
2024-04-09 12:35:36 +0900

    [Gold V] Title: 꿀 따기, Time: 8 ms, Memory: 2412 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Gold/21758. 꿀 따기/꿀 따기.cc
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int N, ar[100001], honey[5], a;
+
+int main()
+{
+    int max = -99999;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> a;
+        if (i != 0 && i != N - 1 && max < a)
+            max = a;
+        if (i == 0)
+            ar[i] = a;
+        else
+            ar[i] += ar[i - 1] + a;
+    }
+    honey[1] = ar[N - 2] - ar[0] + max;
+
+    max = -99999;
+    for (int i = 1; i < N - 1; i++)
+    {
+        int tmp = ar[N - 1] - ar[i] * 2 + ar[i - 1];
+
+        if (max < tmp)
+            max = tmp;
+    }
+    honey[0] = max + ar[N - 1] - ar[0];
+
+    max = -99999;
+
+    for (int i = 1; i < N - 1; i++)
+    {
+        int tmp = ar[i - 1] * 2 - ar[i];
+        if (max < tmp)
+            max = tmp;
+    }
+    honey[2] = ar[N - 2] + max;
+
+    max = -99999;
+    for (int i = 0; i < 3; i++)
+
+        if (max < honey[i])
+            max = honey[i];
+    cout << max;
+}
