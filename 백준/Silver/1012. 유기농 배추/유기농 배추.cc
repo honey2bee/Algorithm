956ae5d2e5727f0a6fd9b2c6b22bb3b8cebbf51e
honey2bee <100987659+honey2bee@users.noreply.github.com>
2024-04-14 14:08:06 +0900

    [Silver II] Title: 유기농 배추, Time: 4 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -1,14 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-int N, M, ar[105][105], visited[105][105];
+int N, M, ar[55][55];
 int T, K, a, b;
 const int dx[] = {0, -1, 0, 1};
 const int dy[] = {-1, 0, 1, 0};
-string s;
 
 void DFS(int x, int y)
 {
-    visited[x][y] = 1;
+    ar[x][y] = 0;
     for (int i = 0; i < 4; i++)
     {
         int nx = x + dx[i];
@@ -16,7 +15,7 @@ void DFS(int x, int y)
 
         if (nx >= N || ny >= M || nx < 0 || ny < 0)
             continue;
-        if (visited[nx][ny] == 0 && ar[nx][ny])
+        if (ar[nx][ny])
             DFS(nx, ny);
     }
 }
@@ -36,18 +35,12 @@ int main()
         for (int i = 0; i < N; i++)
             for (int j = 0; j < M; j++)
             {
-                if (visited[i][j] == 0 && ar[i][j])
+                if (ar[i][j])
                 {
                     DFS(i, j);
                     n++;
                 }
             }
         cout << n << "\n";
-        for (int i = 0; i < N; i++)
-            for (int j = 0; j < M; j++)
-            {
-                ar[i][j] = 0;
-                visited[i][j] = 0;
-            }
     }
 }
